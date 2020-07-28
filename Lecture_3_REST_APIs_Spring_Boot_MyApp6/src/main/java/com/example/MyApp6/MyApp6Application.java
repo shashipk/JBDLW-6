fdf211051407557af705fca198d0f35e88462b8d
Shashi Bhushan Kumar <shashibk1993@gmail.com>
2020-07-28 23:11:16 +0530

    added the code for 3rd lecture

---
+++ Lecture_3_REST_APIs_Spring_Boot_MyApp6/src/main/java/com/example/MyApp6/MyApp6Application.java
@@ -0,0 +1,14 @@
+package com.example.MyApp6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApp6Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MyApp6Application.class, args);
+	}
+
+}
---
+++ Lecture_3_REST_APIs_Spring_Boot_MyApp6/src/test/java/com/example/MyApp6/MyApp6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyApp6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApp6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
